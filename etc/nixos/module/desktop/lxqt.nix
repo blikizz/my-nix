df4ff4aa5c7c6d6411bd229e21ce1a3efad7b574
blikizz <vincentsuryanata7431@gmail.com>
2026-04-21 12:42:26 +0700

    Update lxqt.nix

--- etc/nixos/module/desktop/lxqt.nix
+++ etc/nixos/module/desktop/lxqt.nix
@@ -4,4 +4,11 @@
   services.displayManager.ly.enable = true;
   services.xserver.enable = true;
   services.xserver.desktopManager.lxqt.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    nwg-look
+    libsForQt5.qt5ct
+    kdePackages.qt6ct
+    nwg-displays
+  ];
 }
