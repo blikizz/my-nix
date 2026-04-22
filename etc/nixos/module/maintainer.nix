19783129a9c35a53700665995acd1beb5caca88d
blikizz <vincentsuryanata7431@gmail.com>
2026-04-22 14:19:51 +0700

    Update maintainer.nix

--- etc/nixos/module/maintainer.nix
+++ etc/nixos/module/maintainer.nix
@@ -1,13 +1,21 @@
 { config, pkgs, ...}:
 
+let
+  nix-gc-env = builtins.fetchGit {
+    url = "https://github.com/Julow/nix-gc-env";
+    rev = "4753f3c95891b711e29cb6a256807d22e16cf9cd"; # Use the latest stable rev
+  };
+in
 {
+  imports = [
+    (import "${nix-gc-env}/nix_gc_env.nix")
+  ];
+
   nixpkgs.config.allowUnfree = true;
   nix.gc = {
     automatic = true;
     dates = "weekly";
-    options = "--delete-older-than 7d"; 
-    delete_generations = "+5";
   };
-  
+  nix.gc.delete_generations = "+7";
   nix.settings.auto-optimise-store = true;
 }
