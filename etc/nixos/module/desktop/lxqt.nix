a9392fa21032e83f20fafc5162b5f1f61fef89b8
blikizz <vincentsuryanata7431@gmail.com>
2026-04-22 13:57:50 +0700

    Update lxqt.nix

--- etc/nixos/module/desktop/lxqt.nix
+++ etc/nixos/module/desktop/lxqt.nix
@@ -5,6 +5,13 @@
   services.xserver.enable = true;
   services.xserver.desktopManager.lxqt.enable = true;
 
+  environment.lxqt.excludePackages = with pkgs; 
+  [ 
+   pcmanfm-qt
+   xterm
+   lxqt-archiver
+  ]
+
   environment.systemPackages = with pkgs; [
     nwg-look
     libsForQt5.qt5ct
